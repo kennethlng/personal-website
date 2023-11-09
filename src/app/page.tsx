ec1c121f4a6d660543ebac03e47682a5ae2aa7e7
kennethlng <kennethlng@users.noreply.github.com>
2023-11-09 08:34:44 -0800

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <AspectRatio ratio={16 / 9} className="bg-muted">
-      <Image
-        src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-        alt="Photo by Drew Beamer"
-        fill
-        className="rounded-md object-cover"
-      />
+    <AspectRatio ratio={21 / 9} className="bg-muted relative">
+      <div className="absolute right-0 bottom-0 m-10">
+        <div className="flex items-end space-x-2">
+          <p className="font-bold text-lg">I&apos;m still working on this...</p>
+          <Image src="/memoji_clouds.png" alt="" width={100} height={100} />
+        </div>
+      </div>
     </AspectRatio>
   );
 }
