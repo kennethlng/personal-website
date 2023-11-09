79bd3fc46a7eefe51a3792e3e476037c098b021d
kennethlng <kennethlng@users.noreply.github.com>
2023-11-09 08:34:11 -0800

    Update social-links.tsx

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -1,10 +1,21 @@
 "use client";
 
-import { GithubLogo, InstagramLogo } from "@phosphor-icons/react";
+import {
+  GithubLogo,
+  InstagramLogo,
+  PaperPlaneTilt,
+} from "@phosphor-icons/react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { ICON_FONT_WEIGHT } from "@/constants/icons";
 
 const links = [
+  {
+    name: "Email",
+    href: "mailto:hi@kennethlng.com",
+    icon: PaperPlaneTilt,
+  },
   {
     name: "Github",
     href: process.env.NEXT_PUBLIC_GITHUB_URL ?? "",
@@ -21,12 +32,21 @@ export function SocialLinks() {
   return (
     <div className="flex space-x-4">
       {links.map((item) => (
-        <Button asChild key={item.name} variant="ghost" size="icon">
-          <Link href={item.href}>
-            <span className="sr-only">{item.name}</span>
-            <item.icon className="h-6 w-6" aria-hidden="true" weight="bold" />
-          </Link>
-        </Button>
+        <Tooltip key={item.name}>
+          <TooltipTrigger asChild>
+            <Button asChild key={item.name} variant="ghost" size="icon">
+              <Link href={item.href}>
+                <span className="sr-only">{item.name}</span>
+                <item.icon
+                  className="h-6 w-6"
+                  aria-hidden="true"
+                  weight={ICON_FONT_WEIGHT}
+                />
+              </Link>
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>{item.name}</TooltipContent>
+        </Tooltip>
       ))}
     </div>
   );
